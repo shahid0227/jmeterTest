b8997d59c18ae9211241633fa12ead06fb1a9bad
shahid0227 <islamsmohammed.10@gmail.com>
2016-02-29 22:28:02 -0800

    first commit

---
+++ src/test/java/com/test/AppTest.java
@@ -0,0 +1,20 @@
+package com.test;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class AppTest {
+	
+	@Test
+	public void  testOpenWeb(){
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.facebook.com");
+		
+		
+	
+	
+	}
+
+}
